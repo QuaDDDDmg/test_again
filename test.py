04b2e623de4959e6fbd5fc2777f92cbf7bec096e
QuaDDDDmg <ah.paillaman.o@gmail.com>
2022-10-02 23:01:48 -0300

    Agrego la funcion que faltaba hacer

--- test.py
+++ test.py
@@ -4,8 +4,13 @@ import numpy as np
 def detectar_nulos(df):
     return df.isnull().sum()
 
-    def detectar_nulos(df):
-        return df.isnull().sum()
+def detectar_nulos(df):
+    return df.isnull().sum()
     
-    def calcular_promedios(df):
-        return df.mean()
+def calcular_promedios(df):
+    return df.mean()
+
+def calcular_tipos(df):
+    for col in df.iteritems():
+        print(col.dtypes)
+
